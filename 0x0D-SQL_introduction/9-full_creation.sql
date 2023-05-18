74baa31ae890be2803a1d3ae6f53101a22818570
Mavinkip <mavinngetich1@gmail.com>
2023-05-19 00:08:55 +0300

    second table insetion

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- new TABLE
-CREATE TABLE second_table(id INT,name VARCHAR(256),score int);
- VALUES(1,"John", 10);
- VALUES(2,"Alex",3);
- VALUES(3,"Bob",14);
- VALUES(,4"George",8);
+CREATE TABLE if  not exists second_table(id INT,name VARCHAR(256),score int);
+INSERT into second_table VALUES('1','John', '10');
+INSERT into second_table VALUES('2','Alex','3');
+INSERT into second_table VALUES('3','Bob','14');
+INSERT into second_table VALUES('4','George','8');
