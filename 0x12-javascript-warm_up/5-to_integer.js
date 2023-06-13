4374afb190bf205a7d1bbf0f0b7da6e392a81d75
Mavinkip <mavinngetich1@gmail.com>
2023-06-14 01:05:12 +0300

    not number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const number = parseInt(process.argv[2]);
 const k = process.argv;
-if (!isNaN(number)) {
-    console.log(`My number: ${number}`);
+if (Number(k[2])) {
+  console.log(`My number: ${number}`);
 } else {
-    console.log("Not a number");
+  console.log('Not a number');
 }
