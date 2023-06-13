a8565dd2407240bf452fb0f7f3a32fe10ad4a08a
Mavinkip <mavinngetich1@gmail.com>
2023-06-13 21:49:43 +0300

    say

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
-const m = 'C is fun';
-const y = 'Python is cool';
-const v = 'JavaScript is amazing';
-console.log(m);
-console.log(y);
-console.log(v);
+const txt = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(txt);
