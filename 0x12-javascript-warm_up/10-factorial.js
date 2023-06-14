b19f7b8dbb82d4f5ab8e6ff1042e02752949a3d0
Mavinkip <mavinngetich1@gmail.com>
2023-06-14 03:37:30 +0300

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,16 +1,14 @@
 #!/usr/bin/node
-function factorial(n) {
-if (isNaN(n)) {
+function factorial (n) {
+  if (isNaN(n)) {
     return 1;
-}
+  }
 
-if (n === 0 || n === 1) {
+  if (n === 0 || n === 1) {
     return 1;
-}
+  }
 
-return n * factorial(n - 1);
+  return n * factorial(n - 1);
 }
-
 const number = parseInt(process.argv[2]);
-
-console.log(factorial(number)); 
+console.log(factorial(number));
