ec3dda4b6ebab5e2b793c75d7c6b691cb5036cae
Mavinkip <mavinngetich1@gmail.com>
2023-06-14 03:46:56 +0300

    12

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const args = process.argv.slice(2).map(Number);
+
+if (args.length === 0 || args.length === 1) {
+  console.log(0);
+} else {
+  let largest = args[0];
+  let secondLargest = args[0];
+
+  for (let i = 1; i < args.length; i++) {
+    if (args[i] === 12) {
+      args[i] = 89;
+    }
+
+    if (args[i] > largest) {
+      secondLargest = largest;
+      largest = args[i];
+    } else if (args[i] > secondLargest && args[i] !== largest) {
+      secondLargest = args[i];
+    }
+  }
+
+  console.log(secondLargest);
+}
