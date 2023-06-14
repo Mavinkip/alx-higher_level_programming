62c4aa28bcb827d19f7012cc0ad7ec65be533d90
Mavinkip <mavinngetich1@gmail.com>
2023-06-14 03:48:33 +0300

    12

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,24 +1,8 @@
 #!/usr/bin/node
-const args = process.argv.slice(2).map(Number);
-
-if (args.length === 0 || args.length === 1) {
-  console.log(0);
-} else {
-  let largest = args[0];
-  let secondLargest = args[0];
-
-  for (let i = 1; i < args.length; i++) {
-    if (args[i] === 12) {
-      args[i] = 89;
-    }
-
-    if (args[i] > largest) {
-      secondLargest = largest;
-      largest = args[i];
-    } else if (args[i] > secondLargest && args[i] !== largest) {
-      secondLargest = args[i];
-    }
-  }
-
-  console.log(secondLargest);
-}
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
