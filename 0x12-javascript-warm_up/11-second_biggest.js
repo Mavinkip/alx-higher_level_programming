de0408804267685707ad6402f26933bed708ac58
Mavinkip <mavinngetich1@gmail.com>
2023-06-14 03:43:12 +0300

    11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,4 @@
 #!/usr/bin/node
-const args = process.argv.slice(2).map(Number);
-
-if (args.length === 0 || args.length === 1) {
-  console.log(0);
-} else {
-  let largest = args[0];
-  let secondLargest = args[0];
-
-  for (let i = 1; i < args.length; i++) {
-    if (args[i] > largest) {
-      secondLargest = largest;
-      largest = args[i];
-    } else if (args[i] > secondLargest && args[i] !== largest) {
-      secondLargest = args[i];
-    }
-  }
-
-  console.log(secondLargest);
-}
+const num = process.argv.splice(2).map((z) => parseInt(z)).sort((a, b) => (a - b));
+if (num.length < 2) console.log(0);
+else console.log(num[num.length - 2]);
